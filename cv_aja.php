4cb9bbe87822816a6b71a77720c673824b9a0b3b
dyahcandradewi123 <dyahcandradewi@student.ub.ac.id>
2025-04-13 20:04:27 +0700

    Add files via upload

---
+++ cv_aja.php
@@ -0,0 +1,83 @@
+<!-- HTML DYAH CANDRA DEWI_235150401111064 -->
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Dyah Candra Dewi</title>
+    <link rel="stylesheet" href="cv_aja.css">
+</head>
+<body>
+    <div class="resume">
+        <div class="header">
+            <div>
+                <h1>Dyah Candra Dewi</h1>
+                <p>UX Researcher</p>
+                <div class="contact">
+                    <p><img src="images/gmaps.png" class="icon"> Malang, Indonesia</p>
+                    <p><img src="images/wa.png" class="icon"> 085707610346</p>
+                    <p><img src="images/email.webp" class="icon"> dyahcandradewi@student.ub.ac.id</p>
+                </div>
+            </div>
+            <span class ="foto-ktm">
+                <img src ="images/FOTO DYAH.jpg" alt="Foto KTM">
+            </span>
+        </div>
+
+        <div class="section">
+            <h2>Ringkasan</h2>
+            <p>Seorang mahasiswi prodi Sistem Informasi FILKOM UB yang memiliki kemampuan dalam bidang riset dan ilmiah. Saat ini tengah berfokus pada bidang UI/UX</p>
+        </div>
+
+        <div class="section">
+            <h2>Keterampilan</h2>
+            <ul>
+                <li>Statistika</li>
+                <li>Sistem Analisis Data Kualitatif dan Kuantitatif</li>
+                <li>Data Visualization</li>
+                <li>Wireframe & Prototyping</li>
+                <li>HTML, CSS & JS</li>
+                <li>SQL</li>
+            </ul>
+        </div>
+
+        <div class="section">
+            <h2>Pendidikan</h2>
+            <ul>
+                <li>SDN 1 Kampungdalem Tulungagung</li>
+                <li>SMPN 1 Tulungagung</li>
+                <li>SMAN 1 Kedungwaru Tulungagung</li>
+                <li>Fakultas Ilmu Komputer Universitas Brawajiaya</li>
+            </ul>       
+        </div>
+
+        <div class="section">
+            <h2>Pengalaman Organisasi</h2>
+            <h3>1. KBMDSI (Keluarga Besar Mahasiswa Departemen Sistem Informasi) [2024 - 2025]</h3>
+            <h4>Departemen Medkominfo (Media, Komunikasi, dan Informasi)</h4>
+            <ul>
+                <li>Ketua Pelaksana Studi Banding KBMDSI UB dengan HMD TEP UM</li>
+                <li>Ketua Pelaksana Studi Banding KBMDSI UB dengan KMTETI UGM</li>
+                <li>Ketua Pelaksana Studi Banding KBMDSI UB dengan HIMASKOM UNDIP</li>
+                <li>Sosial Media Specialist KMBDSI UB 2024</li>
+            </ul>
+
+            <h3>2. K-RISMA (Kelompok Riset Mahasiswa) [2024 - sekarang]</h3>
+            <h4>Divisi Perpen</h4>
+            <ul>
+                <li>Staff divisi acara K-RISMA Mini Course 2024</li>
+                <li>Sekretaris K-RISMA XII BOOTCAMP 2024</li>
+            </ul>
+            <h4>Wakil Ketua UMUM K-RISMA Periode XIII Tahun 2025</h4></h4>
+        </div>
+        <div class="section">
+            <h2>Kejuaraan Kompetisi</h2>
+            <ul>
+                <li>Finalis 4C National Competition 2024 bidang Poster Infografis</li>
+                <li>Peraih Pendanaan PKM bidang Kewirausahaan Tahun 2024</li>
+                <li>Juara 3 UI/UX DevGirls Competition 2024</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
